c62a74b79616ceed08a81ae55e65100fd37506dc
LebedevIgor <igorlebedev018@gmail.com>
2023-10-29 19:04:10 +0500

    Task5

---
+++ Tyuiu.LebedevIV.Sprint3.Task5.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LebedevIV.Sprint3.Task5.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LebedevIV.Sprint3.Task5.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61861570-990a-4184-9388-966a089d34e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
